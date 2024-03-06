cda418b8de8e817ff62bc8f8d00d0b3e9ded523d
Rizwan Karim Rafat <53152253+rizwankarimrafat@users.noreply.github.com>
2024-03-06 22:10:02 +1100

    mobile

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+// JavaScript function to scroll to the top of the page
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
