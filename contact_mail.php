aa28d6d26a849f0524bfedac55b721fef925dbdb
Rizwan Karim Rafat <53152253+rizwankarimrafat@users.noreply.github.com>
2024-03-19 20:20:22 +1100

    contact form test update

--- contact_mail.php
+++ contact_mail.php
@@ -11,12 +11,13 @@ require 'vendor/autoload.php';
 //get data from form
 
 $name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
+$number = $_POST['number'];
+$complaintype = $_POST['complaintype'];
+$date = $_POST['date'];
 $message = $_POST['message'];
 
 // preparing mail content
-$messagecontent ="Name: ". $name . "<br><br>E-mail: " . $email . "<br><br>Message: " . $message;
+$messagecontent ="Name: ". $name . "<br><br>Contact: " . $number . "<br><br>Complain type: ". $complaintype . "<br><br>Date of incidence: ". $date . "<br><br>Complain: " . $message;
 
 
 //Create an instance; passing `true` enables exceptions
@@ -30,16 +31,23 @@ try {
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'chowdhuryshaheb66@gmail.com';                     //SMTP username
     $mail->Password   = 'echykhkymouivceo';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     //Recipients
-    $mail->setFrom('chowdhuryshaheb66@gmail.com', 'Portfolio Website / Contact Form');
-    $mail->addAddress('rafat@duck.com');     //Add a recipient
-    //$mail->addReplyTo('head@karimmotors.com.bd');
-    
+    $mail->setFrom('complain@karimmotors.com.bd', 'Customer Complain');
+    $mail->addAddress('head@karimmotors.com.bd');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('head@karimmotors.com.bd');
+    // $mail->addCC('sales@karimmotors.com.bd');
+    // $mail->addCC('service@karimmotors.com.bd');
+    // $mail->addCC('head@karimmotors.com.bd');
+    //$mail->addBCC('bcc@example.com');
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('photo.jpeg', 'photo.jpeg');    //Optional name
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Portfolio Website / Contact Form'' | Subject: '.$subject;
+    $mail->Subject = 'New '.$complaintype.' Complain by '.$name;
     $mail->Body    = $messagecontent;
     
 
